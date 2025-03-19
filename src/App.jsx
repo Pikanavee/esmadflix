3e1e941ddc291d89162e88d2f5bd6c667afb0225
LUISA FIAMENGUI <luisa.fiamengui@aluno.senai.br>
2025-03-19 16:13:45 -0300

    Iniciando o Projeto

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.js";
+import "./App.css";
+
+const App = () => {
+  return <div className="btn btn-danger">App</div>;
+};
+
+export default App;
