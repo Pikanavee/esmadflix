8f01beecbbd42efb9ea7a89c785790d40f57a67d
LUISA FIAMENGUI <luisa.fiamengui@aluno.senai.br>
2025-03-19 16:34:51 -0300

    Inclusão das Imagens e Componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.js";
 import "./App.css";
+import Header from "./components/header/Header";
+import MovieCard from "./components/movieCard/MovieCard";
+import Footer from "./components/footer/Footer";
 
 const App = () => {
-  return <div className="btn btn-danger">App</div>;
+  return (
+    <div>
+      <Header />
+      <MovieCard />
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Search from "../search/Search";
+
+const Header = () => {
+  return (
+    <div>
+      Header
+      <Search></Search>
+    </div>
+  );
+};
+
+export default Header;
